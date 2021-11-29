420542993d6c87458a74d2f99722778dbe78a4e1
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-29 15:42:16 +0300

    Fix tests

--- Assets/UGF.WebRequests.Runtime.Tests/Unity/TestUnityWebRequestSender.cs
+++ Assets/UGF.WebRequests.Runtime.Tests/Unity/TestUnityWebRequestSender.cs
@@ -15,7 +15,7 @@ namespace UGF.WebRequests.Runtime.Tests.Unity
         public IEnumerator Get()
         {
             var sender = new UnityWebRequestSender(new UnityWebRequestSenderDescription());
-            var request = new WebRequest(WebRequestMethod.Get, "https://api.bintray.com/npm/unity-game-framework/stable/com.ugf.application");
+            var request = new WebRequest(WebRequestMethod.Get, "https://unitygameframework.jfrog.io/artifactory/api/npm/default/com.ugf.application");
 
             using (new InitializeScope(sender))
             {
