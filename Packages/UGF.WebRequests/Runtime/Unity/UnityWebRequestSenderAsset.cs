030a73695416c20f48db447e822d9b758c649d23
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-03-02 22:19:00 +0300

    Add UnityWebRequestSenderAsset

---
+++ Packages/UGF.WebRequests/Runtime/Unity/UnityWebRequestSenderAsset.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace UGF.WebRequests.Runtime.Unity
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Web/Unity Web Request Sender", order = 2000)]
+    public class UnityWebRequestSenderAsset : WebRequestSenderAsset
+    {
+        [SerializeField] private int m_redirectLimit = 10;
+        [SerializeField] private int m_timeout;
+        [SerializeField] private bool m_useHttpContinue = true;
+
+        public int RedirectLimit { get { return m_redirectLimit; } set { m_redirectLimit = value; } }
+        public int Timeout { get { return m_timeout; } set { m_timeout = value; } }
+        public bool UseHttpContinue { get { return m_useHttpContinue; } set { m_useHttpContinue = value; } }
+
+        protected override IWebRequestSender OnBuild()
+        {
+            var description = new UnityWebRequestSenderDescription
+            {
+                RedirectLimit = m_redirectLimit,
+                Timeout = m_timeout,
+                UseHttpContinue = m_useHttpContinue
+            };
+
+            return new UnityWebRequestSender(description);
+        }
+    }
+}
