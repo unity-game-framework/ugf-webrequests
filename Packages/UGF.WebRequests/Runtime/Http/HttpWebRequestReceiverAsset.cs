47cdbd8e3a7a603905c66bfeaef23aa9ae40d840
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-03-02 22:25:49 +0300

    Add HttpWebRequestReceiverAsset

---
+++ Packages/UGF.WebRequests/Runtime/Http/HttpWebRequestReceiverAsset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UGF.WebRequests.Runtime.Http
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Web/Http Web Request Receiver", order = 2000)]
+    public class HttpWebRequestReceiverAsset : WebRequestReceiverAsset
+    {
+        [SerializeField] private List<string> m_prefixes = new List<string>();
+
+        public List<string> Prefixes { get { return m_prefixes; } }
+
+        protected override IWebRequestReceiver OnBuild()
+        {
+            var description = new HttpWebRequestReceiverDescription();
+
+            for (int i = 0; i < m_prefixes.Count; i++)
+            {
+                description.Prefixes.Add(m_prefixes[i]);
+            }
+
+            return new HttpWebRequestReceiver(description, new WebRequestReceiveHandler());
+        }
+    }
+}
