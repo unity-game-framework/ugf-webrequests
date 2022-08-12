6c240a78a254a4c5f144c94b8e813441f02757d8
Vorobey <vorobyovvorobyov@gmail.com>
2022-08-12 20:03:46 +0300

    Update WebRequestUtility.cs

--- Packages/UGF.WebRequests/Runtime/WebRequestUtility.cs
+++ Packages/UGF.WebRequests/Runtime/WebRequestUtility.cs
@@ -37,5 +37,95 @@ namespace UGF.WebRequests.Runtime
                 }
             }
         }
+
+        public static bool TryParsCookie(string value, out WebCookie cookie)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentException("Value cannot be null or empty.", nameof(value));
+
+            string[] parts = value.Split(';');
+            string[] nameAndValue = parts[0].Split('=');
+
+            if (nameAndValue.Length > 0 && !string.IsNullOrEmpty(nameAndValue[0]))
+            {
+                cookie = nameAndValue.Length > 1
+                    ? new WebCookie(nameAndValue[0], nameAndValue[1])
+                    : new WebCookie(nameAndValue[0]);
+
+                foreach (string part in parts[1..])
+                {
+                    string[] values = part.Split('=');
+                }
+            }
+
+            cookie = default;
+            return false;
+        }
+
+        public static bool IsValidCookieName(string name)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentException("Value cannot be null or empty.", nameof(name));
+
+            foreach (char character in name)
+            {
+                switch (character)
+                {
+                    case ' ':
+                    case '(':
+                    case ')':
+                    case '<':
+                    case '>':
+                    case '@':
+                    case ',':
+                    case ';':
+                    case ':':
+                    case '\\':
+                    case '"':
+                    case '/':
+                    case '[':
+                    case ']':
+                    case '?':
+                    case '=':
+                    case '{':
+                    case '}':
+                    {
+                        return false;
+                    }
+                }
+
+                if (char.IsControl(character) || char.IsWhiteSpace(character))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsValidCookieValue(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentException("Value cannot be null or empty.", nameof(value));
+
+            foreach (char character in value)
+            {
+                switch (character)
+                {
+                    case ' ':
+                    case ',':
+                    case ';':
+                    case '\\':
+                    case '"':
+                    {
+                        return false;
+                    }
+                }
+
+                if (char.IsControl(character) || char.IsWhiteSpace(character))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
